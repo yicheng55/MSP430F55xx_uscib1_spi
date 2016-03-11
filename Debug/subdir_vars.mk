4fe7cc06e3b76ad5284524ab5777e6622e736910
yi-cheng Liao <yich_liao@davicom.com.tw>
2016-03-11 13:57:00 +0800

    Rename project name.

--- Debug/subdir_vars.mk
+++ Debug/subdir_vars.mk
@@ -7,27 +7,27 @@ CMD_SRCS += \
 ../lnk_msp430f5529.cmd 
 
 C_SRCS += \
-../MSP430F55xx_uscia0_spi_09.c \
+../MSP430F55xx_uscib1_spi_09.c \
 ../MX25R1035Fdriver.c 
 
 OBJS += \
-./MSP430F55xx_uscia0_spi_09.obj \
+./MSP430F55xx_uscib1_spi_09.obj \
 ./MX25R1035Fdriver.obj 
 
 C_DEPS += \
-./MSP430F55xx_uscia0_spi_09.pp \
+./MSP430F55xx_uscib1_spi_09.pp \
 ./MX25R1035Fdriver.pp 
 
 C_DEPS__QUOTED += \
-"MSP430F55xx_uscia0_spi_09.pp" \
+"MSP430F55xx_uscib1_spi_09.pp" \
 "MX25R1035Fdriver.pp" 
 
 OBJS__QUOTED += \
-"MSP430F55xx_uscia0_spi_09.obj" \
+"MSP430F55xx_uscib1_spi_09.obj" \
 "MX25R1035Fdriver.obj" 
 
 C_SRCS__QUOTED += \
-"../MSP430F55xx_uscia0_spi_09.c" \
+"../MSP430F55xx_uscib1_spi_09.c" \
 "../MX25R1035Fdriver.c" 
 
 
